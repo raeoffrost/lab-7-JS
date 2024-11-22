33a821980501fac2d7bc5a3eb232ee05c05c2411
raeoffrost <raeoffrost@gmail.com>
2024-11-21 18:27:29 -0600

    Altered mouse event message for clarity

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// mouse event listener on hover
+// mouse event listener on hover/no hover
+
 const me1 = document.getElementById("me-1");
-me1.addEventListener("mouseenter", hello);
-me1.addEventListener("mouseleave", goodbye);
-function hello(){
-    me1.innerText = "Hello";
+me1.addEventListener("mouseover", hover);
+me1.addEventListener("mouseleave", noHover);
+
+function hover(){
+    me1.innerText = "Hover";
 }
-function goodbye(){
-    me1.innerText = "Bye";
+function noHover(){
+    me1.innerText = "No Hover";
 }
