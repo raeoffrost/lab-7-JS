b13f1e88f2817206524ec9d1ff03e9156f763586
raeoffrost <raeoffrost@gmail.com>
2024-11-21 20:03:20 -0600

    Add mouse enter/hover/leave with timeout

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // mouse event listener on hover/no hover
 
 const me1 = document.getElementById("me-1");
+me1.addEventListener("mouseenter", enter);
 me1.addEventListener("mouseover", hover);
-me1.addEventListener("mouseleave", noHover);
-
-function hover(){
-    me1.innerText = "Hover";
+me1.addEventListener("mouseleave", leave);
+function enter(){
+    me1.innerText = "Hover Start";
 }
-function noHover(){
+function hover(){
+    setTimeout(() => {
+        me1.innerText = "Hovering";
+      }, 500);
+}
+function leave(){
     me1.innerText = "No Hover";
 }
